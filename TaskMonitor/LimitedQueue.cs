b0dcf2991dbbfc28ff4697377ad84cce471051a5
giga487 <giga487@hotmail.com>
2025-05-12 09:26:48 +0200

    upgrade al task monitor

---
+++ TaskMonitor/LimitedQueue.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskMonitor.Utility
+{
+    public class LimitedQueue<T>
+    {
+        private readonly int maxSize;
+        private readonly ConcurrentQueue<T> queue;
+        public int Count
+        {
+            get
+            {
+                try
+                {
+                    return queue.Count;
+                }
+                catch (Exception ex)
+                {
+                    return 0;
+                }
+            }
+        }
+        public LimitedQueue(int maxSize)
+        {
+            this.maxSize = maxSize;
+            this.queue = new ConcurrentQueue<T>();
+        }
+
+        public void Add(T item)
+        {
+            if (queue.Count >= maxSize)
+            {
+                queue.TryDequeue(out var _); // Rimuove l'elemento più vecchio
+            }
+
+            queue.Enqueue(item);
+        }
+
+        public double GetSumOrList()
+        {
+            if (typeof(T).IsNumericType())
+            {
+                try
+                {
+                    return queue.Sum(item => Convert.ToDouble(item));
+                }
+                catch
+                {
+                    return 0;
+                }
+            }
+
+            return 0;
+        }
+
+        public IEnumerable<T> GetItems()
+        {
+            return queue;
+        }
+    }
+    public static class TypeExtensions
+    {
+        public static bool IsNumericType(this Type type)
+        {
+            if (type == null)
+            {
+                return false;
+            }
+
+            switch (Type.GetTypeCode(type))
+            {
+                case TypeCode.Byte:
+                case TypeCode.SByte:
+                case TypeCode.UInt16:
+                case TypeCode.UInt32:
+                case TypeCode.UInt64:
+                case TypeCode.Int16:
+                case TypeCode.Int32:
+                case TypeCode.Int64:
+                case TypeCode.Decimal:
+                case TypeCode.Double:
+                case TypeCode.Single:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
---
+++ TaskMonitor/TaskMonitor.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskMonitor.Utility;
+
+namespace TaskMonitor
+{
+    public class TaskMonitor
+    {
+        public enum TypeOfTask
+        {
+            Default,
+            AsyncUnaryClient,
+            AsyncServerStreaming,
+            Duplex,
+            AsyncClientStreaming,
+            ClientStreamingServer,
+            AsyncUnaryServer,
+            AsyncUpdating,
+            HTC,
+            DispatchingMultiClient,
+            DVWCalculus
+        }
+
+        public TaskMonitor(int millisecondForPolling)
+        {
+            TimeMSToShow = millisecondForPolling;
+
+            foreach (TypeOfTask t in System.Enum.GetValues(typeof(TypeOfTask)))
+            {
+                _processAverageTime[t] = new();
+
+            }
+        }
+
+        public bool ToShow(TypeOfTask t)
+        {
+            if (t != TypeOfTask.Default && t != TypeOfTask.AsyncServerStreaming)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        public record TaskObject
+        {
+            public Task? Task { get; set; }
+            public int UnivoqueID { get; set; }
+            public string? CreatedBy { get; set; }
+            //public Task? Task { get; set; } = null;
+            public TypeOfTask Type { get; set; } = TypeOfTask.Default;
+            public string MethodReq { get; set; } = string.Empty;
+        }
+
+        public record TaskTypeInfo
+        {
+            /// <summary>
+            /// AVERAGE WINDOWED VALUE
+            /// </summary>
+            public static int MaxItems = 100;
+            private LimitedQueue<double> MemorizedValues { get; set; }
+            public int Count { get; set; } = 0;
+            private double _average = 0;
+            public double Average => _average;
+            public TaskTypeInfo()
+            {
+                MemorizedValues = new LimitedQueue<double>(MaxItems);
+            }
+            public void AddNewTicks(double milliseconds)
+            {
+                MemorizedValues.Add(milliseconds);
+
+                try
+                {
+                    _average = MemorizedValues.GetSumOrList() / MemorizedValues.Count; /// MemorizedValues.Count;
+                }
+                catch (Exception e)
+                {
+
+                }
+
+                Count++;
+            }
+
+            private double _lastAverage = 0;
+            public double LastAverage => _lastAverage;
+            public double PercentageChange => (100 * (_average - _lastAverage) / _average);
+
+            public void SetLast()
+            {
+                _lastAverage = _average;
+            }
+        }
+
+        private ConcurrentDictionary<int, TaskObject> _tasks = new ConcurrentDictionary<int, TaskObject>();
+        private ConcurrentDictionary<TypeOfTask, ConcurrentDictionary<string, TaskTypeInfo>> _processAverageTime { get; set; } = new ConcurrentDictionary<TypeOfTask, ConcurrentDictionary<string, TaskTypeInfo>>();
+
+        public int TimeMSToShow { get; private set; } = 60000;
+        public Serilog.ILogger? Logger { get; set; } = null;
+        public string MonitorName { get; set; } = "Default monitor";
+
+        private List<int> ColumnsWidth { get; set; } = new List<int>()
+        {
+            20,
+            40,
+            10,
+            10,
+            14
+        };
+
+
+        private string FillText(char t, int columnIndex)
+        {
+            return new string(t, ColumnsWidth[columnIndex]);
+        }
+        private string PadRightText(string text, int columnIndex)
+        {
+            var stringToShow = text;
+
+            int maxLength = ColumnsWidth[columnIndex];
+            try
+            {
+                if (stringToShow.Length > maxLength)
+                {
+                    int startedIndex = stringToShow.Length - maxLength + 3;
+                    stringToShow = "..." + stringToShow.Substring(startedIndex);
+                }
+                stringToShow = stringToShow.PadRight(maxLength);
+            }
+            catch (Exception)
+            {
+                stringToShow = text;
+            }
+
+            return stringToShow;
+        }
+
+        public async void ShowMonitoredObject(CancellationToken token)
+        {
+            Logger?.ForContext(Serilog.Core.Constants.SourceContextPropertyName, MonitorName);
+
+            int i = 0;
+            while (!token.IsCancellationRequested)
+            {
+                var ts = Stopwatch.StartNew();
+
+                string text = $"\n########################### TABLE FOR TASK MONITOR ############################\n";
+                text += $"########## Active task: [{_tasks.Count}],  Monitored task average process ticks, WINDOWED ITEMS FOR AVG: {TaskTypeInfo.MaxItems} ######\n";
+                text += $"{PadRightText("Type", 0)}| {PadRightText("Method", 1)}| {PadRightText("MilliS", 2)}| {PadRightText("Changed %", 3)}| {PadRightText("Closed Task", 4)}|\n";
+
+                foreach (var typeTask in _processAverageTime)
+                {
+                    if (ToShow(typeTask.Key))
+                    {
+                        foreach (var requestMethodKV in typeTask.Value)
+                        {
+                            if (requestMethodKV.Value is null)
+                            {
+                                continue;
+                            }
+
+                            text += $"{PadRightText(typeTask.Key.ToString(), 0)}| {PadRightText(requestMethodKV.Key, 1)}| {PadRightText(requestMethodKV.Value.Average.ToString("F3"), 2)}| {PadRightText(requestMethodKV.Value.PercentageChange.ToString("+000.00;-000.00;000.00"), 3)}| {PadRightText(requestMethodKV.Value.Count.ToString(), 4)}|\n";
+                            requestMethodKV.Value.SetLast();
+                        }
+                    }
+                }
+
+                text += $"{FillText('_', 0)}| {FillText('_', 1)}| {FillText('_', 2)}| {FillText('_', 3)}| {FillText('_', 4)}|\n";
+
+                ts.Stop();
+                text += $"Table creation time {ts.ElapsedMilliseconds}ms";
+                Logger?.Information(text);
+
+
+                await Task.Delay(TimeMSToShow);
+            }
+        }
+
+
+        public void AddTask(IEnumerable<Task> tasks, string createdBy, TypeOfTask type, string methodRequest = "")
+        {
+            Parallel.ForEach(tasks, new ParallelOptions() { MaxDegreeOfParallelism = 100 }, task =>
+            {
+                AddTask(task, createdBy, type, methodRequest);
+            });
+
+        }
+        public volatile int LastId = 0;
+        public ConcurrentBag<int> Removed = new ConcurrentBag<int>();
+        public void AddTask(Task? task, string createdBy, TypeOfTask type, string methodRequest = "")
+        {
+            if (task is null)
+            {
+                return;
+            }
+
+            var taskCreation = DateTime.UtcNow;
+            //task.ContinueWith(t => MeasureTime(t, taskCreation));
+
+            TaskObject taskObject = new TaskObject()
+            {
+                UnivoqueID = LastId++,
+                Task = task,
+                CreatedBy = createdBy,
+                Type = type,
+                MethodReq = methodRequest
+            };
+
+            //task.ContinueWith(t => MeasureTime(t, taskCreation));
+            _tasks.TryAdd(taskObject.UnivoqueID, taskObject);
+
+            task.ContinueWith(t =>
+            {
+                double milliseconds = Math.Round((DateTime.UtcNow - taskCreation).TotalMilliseconds, 3);
+
+                if (!t.IsCompleted)
+                {
+                    return;
+                }
+
+                if (!_tasks.TryRemove(taskObject.UnivoqueID, out var taskObj))
+                {
+                    Logger?.Warning($"Can't remove {t.Id}");
+                    return;
+                }
+                else
+                {
+                    Removed.Add(t.Id);
+                    //t.Dispose();
+                }
+
+                if (!_processAverageTime[taskObj.Type].TryGetValue(taskObj.MethodReq, out var request))
+                {
+                    _processAverageTime[taskObj.Type][taskObj.MethodReq] = new TaskTypeInfo();
+                }
+                if (_processAverageTime[taskObj.Type].ContainsKey(taskObj.MethodReq))
+                    _processAverageTime[taskObj.Type][taskObj.MethodReq].AddNewTicks(milliseconds);
+            });
+
+        }
+
+        private void MeasureTime(Task t, DateTime creation)
+        {
+            double milliseconds = Math.Round((DateTime.UtcNow - creation).TotalMilliseconds, 3);
+            //long ticksAfter = DateTime.UtcNow - creation;
+            if (!_tasks.TryRemove(t.Id, out var taskObj))
+            {
+                Logger?.Warning($"Can't remove {t.Id}");
+                return;
+            }
+            else
+            {
+                //t.Dispose();
+            }
+
+            if (!_processAverageTime[taskObj.Type].TryGetValue(taskObj.MethodReq, out var request))
+            {
+                _processAverageTime[taskObj.Type][taskObj.MethodReq] = new TaskTypeInfo();
+            }
+            else
+            {
+                request.AddNewTicks(milliseconds);
+            }
+
+        }
+
+        private void MeasureTime(Task t)
+        {
+
+        }
+
+        private bool ToCheck(TypeOfTask type)
+        {
+            if (type == TypeOfTask.AsyncServerStreaming || type == TypeOfTask.AsyncUpdating)
+                return true;
+
+            return false;
+        }
+
+        public int GetActiveTaskCount()
+        {
+            return _tasks.Count;
+        }
+    }
+
+}
